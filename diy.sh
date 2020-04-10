21949ddc0a469b736c8512d746c2793654f1955b
flyaway318 <flyaway318@gmail.com>
2020-04-10 17:01:53 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
 #=================================================
+# oaf
+git clone https://github.com/destan19/OpenAppFilter.git package/destan19
+# passwall
+git clone https://github.com/Lienol/openwrt-package package/lienol
+# openclash
+git clone https://github.com/vernesong/OpenClash.git package/vernesong
+# adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/rufengsuixing
+./scripts/feeds update -a
+./scripts/feeds install -a
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.9/g' package/base-files/files/bin/config_generate
