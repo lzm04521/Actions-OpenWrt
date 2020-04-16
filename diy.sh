d8e8a41b94e55cf730d304cc12f010311ea376b0
lzm04521 <lzm04521@126.com>
2020-04-16 20:43:31 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,9 +7,13 @@ git clone https://github.com/destan19/OpenAppFilter.git package/destan19
 git clone https://github.com/Lienol/openwrt-package package/lienol
 # openclash
 git clone https://github.com/vernesong/OpenClash.git package/vernesong
+# ssrplus
+git clone https://github.com/fw876/helloworld package/ssrplus
 # adguardhome
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/rufengsuixing
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome package/rufengsuixing
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
